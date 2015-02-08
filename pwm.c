781fb37922fff149c86abd3189a003868e5969f7
robzyb <robzyb@gmail.com>
2015-02-08 16:49:48 +0900

    Got rid of unneeded code

--- pwm.c
+++ pwm.c
@@ -10,15 +10,12 @@ enum state_enum {
     NIGHT
 };
 
-enum state_enum state;
-
-#define CLOCK_DAY WDTPW + WDTTMSEL + WDTCNTCL + WDTSSEL + WDTIS0
 #define CLOCK_NIGHT WDTPW + WDTTMSEL + WDTCNTCL + WDTSSEL + WDTIS1
 
 int main( void )
 {
     // Stop watchdog timer to prevent time out reset
-    WDTCTL = CLOCK_DAY;
+    WDTCTL = CLOCK_NIGHT;
 
     // TODO: Set the time from Flash
     unix_time = 0;
@@ -32,8 +29,6 @@ int main( void )
     BCSCTL1 &= !XTS; // LFXT1 low frequency mode
     BCSCTL3 |= LFXT1S_2; // LFXT1 = VLO
 
-    state = DAY;
-
     _BIS_SR(LPM3_bits + GIE); // Enter LPM3 w/interrupt
     
 }
@@ -43,26 +38,13 @@ __interrupt void wdttimer(void)
 {
     static int frac_second = 0;
 
-    enum state_enum state_should_be_in;
-    if ((unix_time / 10) % 2 == 0) {
-        state_should_be_in = NIGHT;
-    } else {
-        state_should_be_in = DAY;
-    }
-
-    if (state_should_be_in == NIGHT) {
-        WDTCTL = CLOCK_NIGHT;
-        frac_second++;
-        frac_second = frac_second % 16;
-        if (frac_second == 0) {
-            unix_time++;
-            P1OUT^=BIT0;
-        }
-
-    } else {
-        WDTCTL = CLOCK_DAY;
-        P1OUT = 0;
+    WDTCTL = CLOCK_NIGHT;
+    frac_second++;
+    frac_second = frac_second % 16;
+    if (frac_second == 0) {
         unix_time++;
+        P1OUT^=BIT0;
     }
+
     IFG1&=~WDTIFG;
 }
