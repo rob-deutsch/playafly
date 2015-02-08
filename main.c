143d3eb1ea39f6aea704202e809a7b9d414fb541
robzyb <robzyb@gmail.com>
2015-02-08 14:25:51 +0900

    Checks to see what state it should be in

--- main.c
+++ main.c
@@ -3,37 +3,58 @@
 #include <string.h>
 #include "option.h"
 
+unsigned long unix_time;
+
+enum state_enum {
+    DAY,
+    NIGHT
+};
+
+enum state_enum state;
+
+#define CLOCK_DAY WDTPW + WDTTMSEL + WDTCNTCL + WDTSSEL + WDTIS0
+
 int main( void )
 {
     // Stop watchdog timer to prevent time out reset
-    WDTCTL = WDTPW + WDTTMSEL + WDTCNTCL + WDTSSEL + WDTIS0;
+    WDTCTL = CLOCK_DAY;
+
+    // TODO: Set the time from Flash
+    unix_time = 0;
 
     // Enable the interrupt and set the ports
     IE1=WDTIFG;
-    P1DIR=BIT0+BIT6;
+    P1DIR=BIT0;
     P1OUT=BIT0;
     P1IE=BIT3;
-    
-    //BCSCTL2=DIVS_3;       // slow down the smclk by division to make the blinking slow.details in clock
     
     BCSCTL1 &= !XTS; // LFXT1 low frequency mode
     BCSCTL3 |= LFXT1S_2; // LFXT1 = VLO
 
-    // module tutorial
-    
-    _EINT();
-    
-    
-    // Need at least one of the following:
-    
-    _BIS_SR(LPM3_bits + GIE); // Enter LPM3 w/interrupt
+    state = DAY;
 
-    //while(1);
+    _BIS_SR(LPM3_bits + GIE); // Enter LPM3 w/interrupt
+    
 }
 
 #pragma vector=WDT_VECTOR
 __interrupt void wdttimer(void)
 {
-    P1OUT^=BIT0+BIT6;
+    static int frac_second = 0;
+
+    enum state_enum state_should_be_in;
+    if ((unix_time / 10) % 2 == 0) {
+        state_should_be_in = NIGHT;
+    } else {
+        state_should_be_in = DAY;
+    }
+
+    unix_time++;
+
+    if (state_should_be_in == NIGHT) {
+        P1OUT^=BIT0;
+    } else {
+        P1OUT = 0;
+    }
     IFG1&=~WDTIFG;
 }
