518116e803452bd52d4aa33815d8f94b3eaadce6
robzyb <robzyb@gmail.com>
2015-02-10 22:55:52 +0900

    Keep WDT firing more often, even in day mode

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #include <msp430.h>
 #include <string.h>
 
-unsigned long unix_time;
+// TODO: Set the time in Makefile
+unsigned long unix_time = 0;
+int frac_second = 0;
 
 enum state_enum {
     DAY,
@@ -11,9 +13,6 @@ enum state_enum {
 
 enum state_enum state;
 
-#define CLOCK_DAY WDTPW + WDTTMSEL + WDTCNTCL + WDTSSEL + WDTIS0
-#define CLOCK_NIGHT WDTPW + WDTTMSEL + WDTCNTCL + WDTSSEL + WDTIS1
-
 unsigned int prog[16] = {1,1,2,3,5,9,14,23,36,52,69,86,100,110,117,122};
 //unsigned int prog[16] = {1,9,69,122,115,104,86,63,40,23,12,6,3,1,1,0};
 
@@ -21,9 +20,6 @@ int main( void )
 {
     // Stop watchdog timer to prevent time out reset
     WDTCTL = WDTPW + WDTHOLD;
-
-    // TODO: Set the time from Flash
-    unix_time = 0;
 
     // Setup the timer
     CCR0 = 128-1;             // PWM Period
@@ -41,7 +37,7 @@ int main( void )
 
     // Set the state. Set up WTD. Enter LPM0
     state = NIGHT;
-    WDTCTL = CLOCK_NIGHT;
+    WDTCTL = WDTPW + WDTTMSEL + WDTCNTCL + WDTSSEL + WDTIS1;
     _BIS_SR(LPM0_bits + GIE); // Enter LPM0 w/interrupt
     
 }
@@ -51,8 +47,19 @@ int main( void )
 #pragma vector=WDT_VECTOR
 __interrupt void wdttimer(void)
 {
-    static int frac_second = 0;
     static int direction = 1;
+
+    if (frac_second == 16-1) {
+        direction = -1;
+    } else if (frac_second == 0) {
+        direction = 1;
+    }
+
+    frac_second += direction;
+
+    if (frac_second == 0 || frac_second == 15) {
+        unix_time++;
+    }
 
     enum state_enum state_should_be_in;
     if ((unix_time / 10) % 2 == 0) {
@@ -65,34 +72,15 @@ __interrupt void wdttimer(void)
         state = state_should_be_in;
         if (state_should_be_in == DAY) {
             CCR1 = 0;
-            WDTCTL = CLOCK_DAY;
             _BIC_SR_IRQ(0xFFFF);
             _BIS_SR_IRQ(LPM3_bits + GIE); // Enter LPM3 w/interrupt
         } else if (state_should_be_in == NIGHT) {
-            WDTCTL = CLOCK_NIGHT;
             _BIC_SR_IRQ(0xFFFF);
             _BIS_SR_IRQ(LPM0_bits + GIE); // Enter LPM0 w/interrupt
         }
     }
     
-    if (state == NIGHT) {
+    if (state == NIGHT) CCR1 = prog[frac_second];
 
-        if (frac_second == 16-1) {
-            direction = -1;
-        } else if (frac_second == 0) {
-            direction = 1;
-        }
-
-        frac_second += direction;
-
-        CCR1 = prog[frac_second];
-
-        if (frac_second == 0) {
-            unix_time++;
-        }
-
-    } else if (state == DAY) {
-        unix_time++;
-    }
     IFG1&=~WDTIFG;
 }
